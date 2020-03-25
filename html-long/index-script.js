6ee5e19adfc5f9a1f618b34e7152819eae461e32
ibrahim nobah <ibrahim.nobah1996@gmail.com>
2020-03-25 23:07:16 +0200

    long-html task-1

--- html-long/index-script.js
+++ html-long/index-script.js
@@ -1,18 +1,76 @@
 $(document).ready(function() {
-  $(".patient-edit").hide();
-  $(".user-list").hide();
-  $(".user-edit").hide();
+  $(".user-list-screen").hide();
+  $(".user-edit-screen").hide();
+  $(".patient-edit-screen").hide();
+  $("#found_site").on("change", function() {
+    $(this).val() == "other"
+      ? $("#specify")
+          .closest(".form-group")
+          .show()
+      : $("#specify")
+          .closest(".form-group")
+          .hide();
+  });
+  $(".add ").click(function() {
+    $(".patient-list-screen").hide();
 
-  $("#patient-list ").click(function() {
-    $(".patient-list").toggle();
-  });
-  $("#patient-edit").click(function() {
-    $(".patient-edit").toggle();
-  });
-  $("#user-list ").click(function() {
-    $(".user-list").toggle();
-  });
-  $("#user-edit ").click(function() {
-    $(".user-edit").toggle();
+    $(".patient-edit-screen").show();
   });
 });
+class Patient {
+  constructor(
+    ID,
+    fname,
+    mname,
+    lname,
+    DOB,
+    gender,
+    email,
+    lastCheck,
+    CreatedBy,
+    creationDate,
+    status
+  ) {
+    this.ID = ID;
+    this.fname = fname;
+    this.mname = mname;
+    this.lname = lname;
+    this.DOB = DOB;
+    this.gender = gender;
+    this.email = email;
+    this.lastCheck = lastCheck;
+    this.CreatedBy = CreatedBy;
+    this.creationDate = creationDate;
+    this.status = status;
+  }
+}
+
+class listpatient {
+  constructor() {
+    this.listofpatient = [];
+  }
+
+  intiData() {
+    patientsData.forEach(element => {
+      var patient = new Patient(
+        element.ID,
+        element.fname,
+        element.mname,
+        element.lname,
+        element.DOB,
+        element.gender,
+        element.email,
+        element.lastCheck,
+        element.status,
+        element.Active,
+        element.creationDate,
+        element.CreatedBy
+      );
+
+      this.listofpatient.push(patient);
+    });
+    console.log(this.listofpatient);
+  }
+}
+var x = new listpatient();
+x.intiData();
