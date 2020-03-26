fc4d73732590bb10e62cffbf23de28e29b533b7a
ibrahim nobah <ibrahim.nobah1996@gmail.com>
2020-03-26 03:37:31 +0200

    html-long add-save button

--- html-long/index-script.js
+++ html-long/index-script.js
@@ -2,14 +2,41 @@ $(document).ready(function() {
   $(".user-list-screen").hide();
   $(".user-edit-screen").hide();
   $(".patient-edit-screen").hide();
-  $("#found_site").on("change", function() {
-    $(this).val() == "other"
-      ? $("#specify")
-          .closest(".form-group")
-          .show()
-      : $("#specify")
-          .closest(".form-group")
-          .hide();
+
+  $("#nameform").submit(function() {
+    var fname = document.getElementsByClassName("fname")[0].value;
+    var mname = document.getElementsByClassName("mname")[0].value;
+    var lname = document.getElementsByClassName("lname")[0].value;
+    var Active = true;
+    var email = document.getElementsByClassName("email")[0].value;
+    var DateofBirth = document.getElementsByClassName("DateofBirth")[0].value;
+    var radios = document.getElementsByName("gender");
+    var gender = "";
+    for (var i = 0, length = radios.length; i < length; i++) {
+      if (radios[i].checked) {
+        gender = radios[i].value;
+        break;
+      }
+    }
+    var lastCheck = document.getElementsByClassName("lastCheck")[0].value;
+    var status = document.getElementById("Status").value;
+    var tmp = patientsData.pop();
+    patientsData.push(tmp);
+    var Id = tmp.ID + 1;
+    var newpatient = new Patient(
+      Id,
+      fname,
+      mname,
+      lname,
+      DateofBirth,
+      gender,
+      email,
+      lastCheck,
+      "by anas",
+      Active,
+      lastCheck,
+      status
+    );
   });
   $(".add ").click(function() {
     $(".patient-list-screen").hide();
@@ -29,7 +56,8 @@ class Patient {
     lastCheck,
     CreatedBy,
     creationDate,
-    status
+    status,
+    Active
   ) {
     this.ID = ID;
     this.fname = fname;
@@ -40,6 +68,7 @@ class Patient {
     this.email = email;
     this.lastCheck = lastCheck;
     this.CreatedBy = CreatedBy;
+    this.Active = Active;
     this.creationDate = creationDate;
     this.status = status;
   }
