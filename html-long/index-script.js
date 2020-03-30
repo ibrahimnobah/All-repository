d3d48d6c62335541eef5020d2eff19e70c473085
ibrahim nobah <ibrahim.nobah1996@gmail.com>
2020-03-30 22:28:05 +0300

    make render dynamiclly

--- html-long/index-script.js
+++ html-long/index-script.js
@@ -1,24 +1,10 @@
 $(document).ready(function() {
+  $('[active="false"]').hide();
   $(".toggle-naviation ").click(function() {
-    var id = this.id;
-    switch (id) {
-      case "users-add-naviation":
-        $(".patient-list-screen").hide();
-        $(".user-list-screen").show();
-        $(".patient-edit-screen").hide();
-        break;
-      case "patient-navigation":
-        $(".patient-list-screen").show();
-        $(".user-list-screen").hide();
-        $(".patient-edit-screen").hide();
-        break;
-      case "patient-edit-naviation":
-        $(".patient-list-screen").hide();
-        $(".user-list-screen").hide();
-        $(".patient-edit-screen").show();
-        break;
-      default:
-        alert("");
-    }
+    $('[active="true"]').attr("active", "false");
+    var divToShow = $(this).attr("divToShow");
+    $("." + divToShow).attr("active", "true");
+    $('[active="true"]').show();
+    $('[active="false"]').hide();
   });
 });
